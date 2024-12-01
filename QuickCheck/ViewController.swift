75a88ff5a2702677222e449a3c0c608b9b35a397
saksh2311 <patil.saksh@northeastern.edu>
2024-12-01 13:27:57 -0500

    Initial Commit

---
+++ QuickCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickCheck
+//
+//  Created by Sakshi Patil on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
