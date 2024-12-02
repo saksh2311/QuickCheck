66c9b53dde12c265229e53b8ab1225f01911d44e
vanshita-tilwani <msvtilwani@gmail.com>
2024-12-02 17:31:14 -0500

    Add class controller

--- QuickCheck/TabBarController.swift
+++ QuickCheck/TabBarController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-class TabBarController: UITabBarController {
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
 
+    var CurrentDetails : UIViewController.BasicDetails?
+    
+    // Tutor Tabs
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        self.delegate = self
 
-        // Do any additional setup after loading the view.
+        tabBar.tintColor = UIColor.green
+        tabBar.barTintColor = UIColor.darkText
+        
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
