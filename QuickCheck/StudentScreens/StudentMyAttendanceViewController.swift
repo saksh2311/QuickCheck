03615bfab748270796c5c6485a582e2620b533ac
vanshita-tilwani <msvtilwani@gmail.com>
2024-12-02 21:57:42 -0500

    project files

---
+++ QuickCheck/StudentScreens/StudentMyAttendanceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StudentMyAttendanceViewController.swift
+//  QuickCheck
+//
+//  Created by Vanshita Tilwani on 12/2/24.
+//
+
+import UIKit
+
+class StudentMyAttendanceViewController: UIViewController {
+    
+    var CurrentDetails : UIViewController.BasicDetails?
+    var ViewTitle : String = "My Attendance"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
