a6330c97cbe33b3d78a8b29d75592ffa0870580c
Rumjhum Singru <rumjhumsingru123@gmail.com>
2024-12-03 14:52:23 -0500

    Tab for both users

---
+++ QuickCheck/StudentScreens/StudentGetQRCodeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  StudentGetQRCodeViewController.swift
+//  QuickCheck
+//
+//  Created by Rumjhum Singru on 12/3/24.
+//
+
+import Foundation
--- QuickCheck/TabBarController.swift
+++ QuickCheck/TabBarController.swift
@@ -2,7 +2,7 @@
 //  TabBarController.swift
 //  QuickCheck
 //
-//  Created by Sakshi Patil on 12/1/24.
+//  Created by Rumjhum Singru on 12/3/24.
 //
 
 import UIKit
@@ -11,11 +11,15 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
 
     var CurrentDetails : UIViewController.BasicDetails?
     
-    // Tutor Tabs
+    let classHomeViewController : ClassHomeViewController = ClassHomeViewController()
+    let QRCodeViewController : GenerateQRCodeViewController = GenerateQRCodeViewController()
+    let MyStudentViewController : StudentMyAttendanceViewController = StudentMyAttendanceViewController()
+    let ProfileViewController : MyProfileViewController = MyProfileViewController()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("called")
         view.backgroundColor = UIColor.black
         self.delegate = self
 
@@ -25,6 +29,98 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
     }
 
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+
+        if CurrentDetails?.UserType == "tutors"{
+            initiateTutorTabs()
+        }
+        
+        else if CurrentDetails?.UserType == "students"{
+            initiateStudentTabs()
+        }
+    }
+
+    // Animations
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        let fromView: UIView = tabBarController.selectedViewController!.view
+        let toView: UIView = viewController.view
+        
+        if fromView == toView {
+            return false
+        }
+        
+        if let tappedIndex = tabBarController.viewControllers?.index(of: viewController) {
+            if tappedIndex > tabBarController.selectedIndex {
+                UIView.transition(from: fromView, to: toView, duration: 0.5, options: UIView.AnimationOptions.transitionFlipFromLeft, completion: nil)
+            } else {
+                UIView.transition(from: fromView, to: toView, duration: 0.5, options: UIView.AnimationOptions.transitionFlipFromRight, completion: nil)
+            }
+        }
+        return true
+    }
+    
+    func initiateTutorTabs(){
+        // Class Home viewController
+        classHomeViewController.CurrentDetails = CurrentDetails
+        let classHomeTab = UINavigationController(rootViewController: classHomeViewController)
+        classHomeTab.tabBarItem.title = "Home"
+        classHomeTab.tabBarItem.image = UIImage(systemName: "house.fill")
+
+        
+        // QR Code viewController
+        QRCodeViewController.CurrentDetails = CurrentDetails
+        let qrCodeTab = UINavigationController(rootViewController: QRCodeViewController)
+        qrCodeTab.tabBarItem.title = "Generate QR"
+        qrCodeTab.tabBarItem.image = UIImage(systemName: "qrcode")
+        
+        
+        
+        // My Students viewController
+        MyStudentViewController.CurrentDetails = CurrentDetails
+        let myStudentTab = UINavigationController(rootViewController: MyStudentViewController)
+        myStudentTab.tabBarItem.title = "My Students"
+        myStudentTab.tabBarItem.image = UIImage(systemName: "person.3.fill")
+        
+        
+        
+        // My profile viewController
+        ProfileViewController.CurrentDetails = CurrentDetails
+        let profileTab = UINavigationController(rootViewController: ProfileViewController)
+        profileTab.tabBarItem.title = "My Profile"
+        profileTab.tabBarItem.image = UIImage(systemName: "person.crop.circle")
+        
+        
+        viewControllers = [classHomeTab, qrCodeTab, myStudentTab, profileTab]
+    }
+    
+    
+    func initiateStudentTabs(){
+        
+        let qrScannerViewController : StudentGetQRCodeViewController = StudentGetQRCodeViewController()
+        qrScannerViewController.CurrentDetails = CurrentDetails
+        let qrScannerTab = UINavigationController(rootViewController: qrScannerViewController)
+        qrScannerTab.tabBarItem.title = "Scan QR"
+        qrScannerTab.tabBarItem.image = UIImage(systemName: "qrcode.viewfinder")
+        
+        let myAttendanceViewController : StudentMyAttendanceViewController = StudentMyAttendanceViewController()
+        myAttendanceViewController.CurrentDetails = CurrentDetails
+        myAttendanceViewController.ViewTitle = "My Attendance"
+        
+        let myAttendanceTab = UINavigationController(rootViewController: myAttendanceViewController)
+        myAttendanceTab.tabBarItem.title = "My Attendance"
+        myAttendanceTab.tabBarItem.image = UIImage(systemName: "checkmark.rectangle.fill")
+        
+        
+        ProfileViewController.CurrentDetails = CurrentDetails
+        let profileTab = UINavigationController(rootViewController: ProfileViewController)
+        profileTab.tabBarItem.title = "My Profile"
+        profileTab.tabBarItem.image = UIImage(systemName: "person.crop.circle")
+        
+        
+         viewControllers = [myAttendanceTab, qrScannerTab, profileTab]
+    }
     
 
 }
